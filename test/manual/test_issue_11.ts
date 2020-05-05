e0fbf4d7268422470255c8f3818596d53b39964d
yumauri <yumaa.verdin@gmail.com>
2020-05-05 20:21:09 +0300

    Add filename check errors, fix issue #11

---
+++ test/manual/test_issue_11.ts
@@ -0,0 +1,16 @@
+import { createWriteStream, readFileSync } from 'fs'
+import { convert, gotenberg, office, pipe, please } from '../../src'
+
+// need to run Gotenberg like this
+// docker run --rm -p 3500:3000 thecodingmachine/gotenberg:6
+
+const buffer = readFileSync('document.docx')
+
+pipe(
+  gotenberg('http://localhost:3500'),
+  convert,
+  office,
+  please
+)(['aaaa', buffer])
+  .then(pdf => pdf.pipe(createWriteStream(`${__dirname}/document.pdf`)))
+  .catch(console.error)
