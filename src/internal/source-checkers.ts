84cc655be34aacfe7ba2d0f3b95d454404359b91
Hong Xiaojian <hongxiaojian@cel.la>
2020-09-05 18:58:28 +0800

    fix: disable tslint for unicode regexp

--- src/internal/source-checkers.ts
+++ src/internal/source-checkers.ts
@@ -82,6 +82,7 @@ export const isIterable = (
  * Check, if given argument is simple string, and presumably is is just file name
  * I hope no one will wants to use filename longer than 50 symbols :)
  */
+/* tslint:disable:no-empty-character-class */
 const fileNameRE = /^[\p{L}\w\s\(\),-]+\.[A-Za-z0-9]+$/u
 const MIN_FILE_NAME_LENGTH = 3
 const MAX_FILE_NAME_LENGTH = 50
